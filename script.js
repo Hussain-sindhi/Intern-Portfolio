8fbd0150e6775e44fcae05e893d328b60f3a786e
khanhussain4129-ops <khanhussain4129@gmail.com>
2026-09-08 09:01:36 +0530

    First.js_program

---
+++ script.js
@@ -0,0 +1,42 @@
+// For loop and if-else condition
+let a = 1 
+for(a = 1; a <= 10; a++) {
+    if(a%2 == 0) {
+        console.log("even")
+    } else {
+
+            console.log("odd")
+        }
+    console.log(a)
+}
+
+// print 9 to 5 in decreasing order
+for(a=9; a >= 5; a--) {
+    console.log(a)
+}
+
+
+See what happen
+console.log(a)
+console.log(b)
+console.log(c)
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+
+var a = 10;
+let b = 20;
+const c = 30;
+
+b= 40;
+c =20; // error bcz const variable cannot be re-assigned
+
+
+if ("5" == 5)
+if ("5" === 5)
+
+console.log("5" + 5)
+console.log(6 + "6")
+console.log("6"+ 7+ 7) // 677 bcz "6"+7= "67" & "67"+7= "677" 
